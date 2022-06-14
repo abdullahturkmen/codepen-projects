40def0aceb699316cfdbe01c96e41b28701f2968
Abdullah Turkmen <abdullahtrkmn13@gmail.com>
2022-06-14 21:46:44 +0300

    js number change animation added

--- positive-negative/assets/js/app.js
+++ positive-negative/assets/js/app.js
@@ -3,16 +3,36 @@ let counterContent = document.querySelector(".counter");
 let angle;
 let changeNumber;
 let newNumber;
+let changingNumber = 0;
 onmousemove = function (e) {
-    let mouseX = e.clientX;
-    // let mouseY = e.clientY;
-    let windowWidth = window.innerWidth;
-    let halfWindowWidh = windowWidth / 2;
+	let mouseX = e.clientX;
+	//let mouseY = e.clientY;
+	let windowWidth = window.innerWidth;
+	let halfWindowWidh = windowWidth / 2;
 
-    // let i = counterContent.innerText;
-    changeNumber = mouseX - halfWindowWidh;
-    newNumber = Math.floor(changeNumber * 2);
-    counterNumber.innerText = newNumber;
-    angle = (changeNumber * 20) / halfWindowWidh;
-    counterContent.style.transform = `rotate(${angle}deg)`;
+	//let i = counterContent.innerText;
+	changeNumber = mouseX - halfWindowWidh;
+	//(console.log(changeNumber)
+	newNumber = Math.floor(changeNumber);
+	setTimeout(function () {
+		changingNumber = newNumber;
+	}, 1000);
+
+	//counterNumber.innerText = newNumber;
+	animateValue(counterNumber, changingNumber, newNumber, 1500);
+	angle = (changeNumber * 20) / halfWindowWidh;
+	counterContent.style.transform = `rotate(${angle}deg)`;
 };
+
+function animateValue(obj, start, end, duration) {
+	let startTimestamp = null;
+	const step = (timestamp) => {
+		if (!startTimestamp) startTimestamp = timestamp;
+		const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+		obj.innerHTML = Math.floor(progress * (end - start) + start);
+		if (progress < 1) {
+			window.requestAnimationFrame(step);
+		}
+	};
+	window.requestAnimationFrame(step);
+}

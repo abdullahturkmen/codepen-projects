019cb73517d23918d1a63ceac0542741e123450e
Abdullah Türkmen <abdullahtrkmn13@gmail.com>
2023-04-29 18:31:51 +0300

    feat: character detail screen added

--- god-of-war-slider/assets/js/app.js
+++ god-of-war-slider/assets/js/app.js
@@ -1,3 +1,27 @@
+
+const charactersList = [
+  { characterName: 'Kratos', characterImg: 'kratos', characterAbout: 'In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.' },
+  { characterName: 'Atreus', characterImg: 'atreus', characterAbout: 'In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.' },
+  { characterName: 'Thor', characterImg: 'thor', characterAbout: 'In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.' },
+  { characterName: 'Angrboda', characterImg: 'angrboda', characterAbout: 'In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.' },
+  { characterName: 'Brok Sindiri', characterImg: 'brok-sindri', characterAbout: 'In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.' },
+  { characterName: 'Mimir', characterImg: 'mimir', characterAbout: 'In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.' },
+  { characterName: 'Tyr', characterImg: 'tyr', characterAbout: 'In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.' },
+  { characterName: 'Freya', characterImg: 'freya', characterAbout: 'In the 1960s, the popularity of Lorem Ipsum increased with the release of Letraset character transfer sheets that included passages of the text. Since then, it has become even more widely used in the design and printing industry, particularly with the advent of desktop publishing software such as Aldus PageMaker, Pages and Microsoft Word, which include versions of the Lorem Ipsum text.' },
+]
+
+document.addEventListener("DOMContentLoaded", function () {
+  var swiperContent = document.querySelector('.swiper-wrapper')
+  charactersList.map((e, index) => {
+    element = `<div class="swiper-slide">
+    <span>${e.characterName}</span>
+    <img src="./assets/img/characters/${e.characterImg}.png" alt="${e.characterName}" title="${e.characterName}">
+    <button class="detail-btn" onclick="characterDetail(${index})">Detail</button>
+  </div>`
+    swiperContent.innerHTML += element
+  })
+});
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
@@ -26,9 +50,20 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
+
+
+const characterDetail = (index) => {
+  const details = document.querySelector('.details')
+  const detailCharImg = document.querySelector('.detail-character-img')
+  const detailCharName = document.querySelector('.detail-character-name')
+  const detailCharAbout = document.querySelector('.detail-character-about')
+  if (index < 0) {
+    return details.classList.remove('active')
+  }
+  var charData = charactersList[index]
+  detailCharImg.src = `./assets/img/characters/${charData.characterImg}.png`
+  detailCharName.innerHTML = charData.characterName
+  detailCharAbout.innerHTML = charData.characterAbout
+  details.classList.add('active')
+}
